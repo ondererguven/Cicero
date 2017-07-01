42120fe4f9e9b25ddd231714d0145f0f730738ff
Onder Erguven <onder.erguven1@gmail.com>
2017-07-01 10:11:39 +0200

    Project setup

---
+++ Cicero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cicero
+//
+//  Created by Onder Erguven on 01/07/2017.
+//  Copyright © 2017 taralli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
