8f98c685e49e6beb16c62277289da82c26be0bb7
Mattia Picariello <mattiapicariello@icloud.com>
2017-07-02 02:01:28 +0200

    change green color in palette

--- Cicero/DesignTool.swift
+++ Cicero/DesignTool.swift
@@ -14,7 +14,7 @@ class Palette {
     static let colorFor = Palette()
     
     let red = UIColor(red: 168/255, green: 37/255, blue: 63/255, alpha: 1)
-    let green = UIColor.green
+    let green = UIColor(red: 54/255, green: 65/255, blue: 31/255, alpha: 1)
     let blue = UIColor(red: 63/255, green: 83/255, blue: 188/255, alpha: 1)
     let lightGray = UIColor(red: 189/255, green: 189/255, blue: 189/255, alpha: 1)
     let darkGray = UIColor(red: 70/255, green: 70/255, blue: 70/255, alpha: 1)
